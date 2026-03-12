c1922dfcce35d2d8d9d161f7f749852e003dbba2
Marko Esna <marko.esna@gmail.com>
2026-03-12 20:34:36 +0200

    feat: added negative tests

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -5,14 +5,27 @@ import { StatusCodes } from 'http-status-codes'
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
+  const responseBody = await response.json()
   // Log the response status, body and headers
-  console.log('response body:', await response.json())
+  console.log('response body:', responseBody)
   console.log('response headers:', response.headers())
   // Check if the response status is 200
   expect(response.status()).toBe(200)
+  // Check that id field value equals 1
+  expect(responseBody.id).toBe(1)
+  // Check that status field value is 'OPEN'
+  expect(responseBody.status).toBe('OPEN')
 })
 
-test('post order with correct data should receive code 201', async ({ request }) => {
+// Negative scenario with invalid order id of 0
+test('get order with incorrect id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
+  // Check if the response status is 400
+  expect(response.status()).toBe(400)
+})
+
+test('post order with correct data should receive code 200', async ({ request }) => {
   // prepare request body
   const requestBody = {
     status: 'OPEN',
@@ -20,7 +33,7 @@ test('post order with correct data should receive code 201', async ({ request })
     customerName: 'string',
     customerPhone: 'string',
     comment: 'string',
-    id: 0,
+    id: 10,
   }
   // Send a POST request to the server
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
@@ -29,5 +42,23 @@ test('post order with correct data should receive code 201', async ({ request })
   // Log the response status and body
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
+  const responseBody = await response.json()
   expect(response.status()).toBe(StatusCodes.OK)
+  expect(responseBody.status).toBe('OPEN')
+})
+
+test('login test', async ({ request }) => {
+  const requestBody = {
+    username: 'asd',
+    password: 'pw1212412',
+  }
+
+  const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
+    data: requestBody,
+  })
+
+  const responseBody = await response.text()
+  console.log('response status:', response.status())
+  console.log('response body:', responseBody)
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
 })
